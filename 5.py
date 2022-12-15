63588e65648eb7c6f59f272d9c88b9668d0b7d48
Deyman13 <118282451+Deyman13@users.noreply.github.com>
2022-12-15 22:41:27 +0300

    Домашка

--- 5.py
+++ 5.py
@@ -33,7 +33,7 @@ print(negafib2(n))
 
 # данное решение выдает результат почти идеальный, за исключением одного - 
 negafib3 = lambda n: n if n < 2 else negafib3(n - 1) + negafib3(n - 2)
-negafib4 = lambda n: negafib3(n) if n >= -1 else round(negafib4(abs(n)) * (-1)**(n + 1))
+negafib4 = lambda n: negafib3(n) if n >= 0 else round(negafib4(abs(n)) * (-1)**(n + 1))
 print([negafib4(num) for num in range(-number, number + 1)])
 
 
